7eab9297c2e2a17f19f2dd1801258a0526ef25d0
Alena Nikitina <alena.tkachenk@mail.ru>
2023-05-26 15:06:34 +0300

    check validation

--- middlewares/routesValidation.js
+++ middlewares/routesValidation.js
@@ -5,9 +5,7 @@ module.exports.sigupValidator = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(2),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(URL_CHECK),
+    name: Joi.string().min(2).max(30).required(),
   }),
 });
 
